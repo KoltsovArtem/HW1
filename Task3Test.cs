a647b2f6f2390596d1a6dd6755d2a4e24ef52f44
KoltsovArtem <81867646+KoltsovArtem@users.noreply.github.com>
2022-09-11 21:38:10 +0300

    Update Task3Test.cs

--- Task3Test.cs
+++ Task3Test.cs
@@ -9,28 +9,32 @@ public class Tests
     public void FTest()
     {
         That(F(0.0), Is.EqualTo(1.0).Within(1e-5));
-        throw new NotImplementedException("Необходимо добавить больше тестов");
+        That(F(3.73772172), Is.EqualTo(-1.0).Within(1e-5));
+        That(F(-1.0), Is.EqualTo(0.0).Within(1e-5));
     }
 
     [Test]
     public void NumberOfDaysTest()
     {
         That(NumberOfDays(2021), Is.EqualTo(365));
-        throw new NotImplementedException("Необходимо добавить больше тестов");
+        That(NumberOfDays(2020), Is.EqualTo(366));
+        That(NumberOfDays(300), Is.EqualTo(365));
     }
 
     [Test]
     public void Rotate2Test()
     {
         That(Rotate2('С', 1, -1), Is.EqualTo('С'));
-        throw new NotImplementedException("Необходимо добавить больше тестов");
+        That(Rotate2('Ю', 2, -1), Is.EqualTo('В'));
+        That(Rotate2('C', 1, 2), Is.EqualTo('В'));
     }
 
     [Test]
     public void AgeDescriptionTest()
     {
         That(AgeDescription(42), Is.EqualTo("сорок два года"));
-        throw new NotImplementedException("Необходимо добавить больше тестов");
+        That(AgeDescription(20), Is.EqualTo("двадцать лет"));
+        That(AgeDescription(27), Is.EqualTo("двадцать семь лет"));
     }
 
     [Test]
