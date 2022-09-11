d7228adb00f59ceaf94569d0f8a6c9a786046d9d
KoltsovArtem <81867646+KoltsovArtem@users.noreply.github.com>
2022-09-11 21:13:25 +0300

    Update Task2Test.cs

--- Task2Test.cs
+++ Task2Test.cs
@@ -14,31 +14,31 @@ public class Tests
     [Test]
     public void Min3Test2()
     {
-        throw new NotImplementedException("Добавьте тестовый случай");
+        That(Min3(6, 3, 3), Is.EqualTo(3));
     }
 
     [Test]
     public void Min3Test3()
     {
-        throw new NotImplementedException("Добавьте тестовый случай");
+        That(Min3(2, -1, 3), Is.EqualTo(-1));
     }
 
     [Test]
     public void Max3Test1()
     {
-        throw new NotImplementedException("Добавьте тестовый случай");
+        That(Max3(4, 6123746128734, 1), Is.EqualTo(6123746128734);
     }
 
     [Test]
     public void Max3Test2()
     {
-        throw new NotImplementedException("Добавьте тестовый случай");
+        That(Max3(4, -6123746128734, 1), Is.EqualTo(4);
     }
 
     [Test]
     public void Max3Test3()
     {
-        throw new NotImplementedException("Добавьте тестовый случай");
+        That(Max3(0, 0, 0), Is.EqualTo(0);
     }
 
     [Test]
@@ -58,7 +58,15 @@ public class Tests
     [Test]
     public void MoreRadDegTests()
     {
-        throw new NotImplementedException(
-            "Напишите для каждой функции `deg2Rad` и `rad2Deg` по 5 тестов, проверяющих разные случаи");
+        That(Deg2Rad(180.0 - 180.0 * 0.5), Is.EqualTo(Math.PI - Math.PI * 0.5).Within(1e-5));
+        That(Deg2Rad(180.0 + 180.0 * 3), Is.EqualTo(Math.PI + Math.PI * 3).Within(1e-5));
+        That(Deg2Rad(180.0 - 180.0 * 0.5 - 180.0 * 0.3), Is.EqualTo(Math.PI - Math.PI * 0.5 - Math.PI * 0.3).Within(1e-5));
+        That(Deg2Rad(180.0 + 180.0 * 3 + 180.0 * 0.5), Is.EqualTo(Math.PI + Math.PI * 3 + Math.PI * 0.5).Within(1e-5));
+        That(Deg2Rad(0), Is.EqualTo(0).Within(1e-5));
+        That(Rad2Deg(Math.PI - Math.PI * 0.5), Is.EqualTo(180.0 - 180.0 * 0.5).Within(1e-5));
+        That(Rad2Deg(Math.PI + Math.PI * 3), Is.EqualTo(180.0 + 180.0 * 3).Within(1e-5));
+        That(Rad2Deg(Math.PI - Math.PI * 0.5 - Math.PI * 0.3), Is.EqualTo(180.0 - 180.0 * 0.5 - 180.0 * 0.3).Within(1e-5));
+        That(Rad2Deg(Math.PI + Math.PI * 3 + Math.PI * 0.5), Is.EqualTo(180.0 + 180.0 * 3 + 180.0 * 0.5).Within(1e-5));
+        That(Rad2Deg(0), Is.EqualTo(0).Within(1e-5));
     }
 }
